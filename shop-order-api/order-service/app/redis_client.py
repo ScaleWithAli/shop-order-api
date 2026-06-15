bb0639e751b149e6bdf1d18735943cdd8cdba84c
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-06-15 10:49:23 +0500

    Update redis_client.py

--- shop-order-api/order-service/app/redis_client.py
+++ shop-order-api/order-service/app/redis_client.py
@@ -2,7 +2,7 @@ import redis
 import json
 from app.config import settings
 
-redis_client = redis.from_url(settings.redis_url, decode_responses=True)
+redis_client = redis.from_url(settings.redis_url, decode_responses=True, ssl_cert_reqs=None)
 
 def publish_order_event(event: str, data: dict):
     payload = json.dumps({"event": event, "data": data})
